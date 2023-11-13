978f8171bfa540642c9df856dc20efc791ca6c02
Osom-Emmanuel <osomemmanuel40@email.com>
2023-11-13 00:55:46 -0800

    created table with inputs from javascript

---
+++ index.js
@@ -0,0 +1,51 @@
+let inputRow = document.getElementById("inputrow");
+let inputsColumn = document.getElementById("inputcol");
+let Table = document.querySelector('.table');
+let Eraser = document.getElementById('eraser');
+let Color = document.querySelector('color');
+let display = document.querySelector('.grid-container');
+let draw = document.querySelector('.Draw');
+
+
+
+
+
+
+// let Tables = document.createElement("table");
+process = () => {
+    let  Row = parseInt(inputRow.value);
+    let Column = parseInt(inputsColumn.value);
+
+    console.log(Row)
+    console.log(Column)
+    
+    for (let i = 0; i < Row; i++){
+        let tableRow = document.createElement("tr");
+        
+        for (let j = 0; j < Column; j++){
+            let tableData = document.createElement("td");
+
+
+            tableRow.appendChild(tableData);
+            Table.appendChild(tableRow);
+        }
+
+        display.innerHTML = "";
+        
+        display.appendChild(Table)
+    }
+    CellArray = document.querySelectorAll("td")
+    console.log(CellArray);
+
+}
+draw.addEventListener('click', process)
+// console.log(process)
+
+
+
+
+
+    for (let p = 0; p < Color; p++) {
+        const element = CellArray[p];
+        
+    }
---
+++ pixel.js
@@ -0,0 +1,20 @@
+let inputRow = document.getElementById("inputrow");
+let inputsColumn = document.getElementById("inputcol");
+let Table = document.querySelector('.table');
+let Eraser = document.getElementById('eraser');
+let Color = document.querySelector('color');
+let display = document.querySelector('.grid-container');
+let draw = document.querySelector('.Draw');
+
+TableArray = () =>{
+    Rows = inputRow.value;
+    Columns = inputsColumn.value;
+
+    for (let i = 0; i < Rows; i++) {
+        let rowLabels = document.createElement("tr")
+
+        
+        
+    }
+
+}
